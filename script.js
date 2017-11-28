a1d5892dacbb62bb55732e81807ee84a0cd9d5af
Wilker-Lucio <wilkerwbl@gmail.com>
2017-11-28 15:47:52 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-documnet.write("Why this bull shit doesn't work?");
+document.write("Why this bull shit doesn't work?");
 //var a = document.getElementById("test").innerHTML = "Share using GitHub :)";
 //a.innerHTML = "Share using GitHub :)";
